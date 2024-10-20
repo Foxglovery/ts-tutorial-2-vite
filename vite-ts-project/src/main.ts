e22401a92d66d684e5c7ac23d409cfc781f9e503
Foxglovery <foxglovery@gmail.com>
2024-10-20 17:03:22 -0500

    added class and data models

--- vite-ts-project/src/main.ts
+++ vite-ts-project/src/main.ts
@@ -1,6 +1,9 @@
 import './css/style.css'
 
-
-
-
-
+// start by creating data model
+// created interface and then class for ListItem
+// created getters and setters for properties
+// did the same for the Full List
+// only having one instance of Full List
+// created all methods for the Full List
+// created templates
